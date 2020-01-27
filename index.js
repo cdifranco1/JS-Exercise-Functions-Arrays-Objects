07b3fb748873de6cce44136731570e425bd3f001
Charlie DiFranco <charlie.difranco2@gmail.com>
2020-01-27 14:32:24 -0600

    finished older cars challenge

--- index.js
+++ index.js
@@ -220,8 +220,12 @@ function sortCarInventory(arr) {
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * getModelYears returns an array containing all the 'car_year's in the inventory.
 */
-function getModelYears(/* code here */) {
-  /* code here */
+function getModelYears(arr) {
+  let result = [];
+  for (i of arr){
+    result.push(i.car_year)
+  }
+  return result;
 }
 
 /**
@@ -236,8 +240,14 @@ function getModelYears(/* code here */) {
  * with a `car_year` which is at most the given desired max year,
  * in the same order as they appear in the original inventory.
 */
-function getOlderCars(/* code here */) {
-  /* code here */
+function getOlderCars(arr, num) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i].car_year <= num){
+      result.push(arr[i])
+    };
+  };
+  return result;
 }
 
 /**
